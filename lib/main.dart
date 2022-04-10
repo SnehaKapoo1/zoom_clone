166c1489509b1976ad0bb07f0a4fb48c38280b98
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-04-10 19:53:57 +0530

    !!

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:zoom_clone/pages/joinmeeting.dart';
-// import 'package:zoom_clone/pages/page1.dart';
-// import 'package:zoom_clone/pages/welcome.dart';
+//import 'package:zoom_clone/pages/joinmeeting.dart';
+import 'package:zoom_clone/pages/loginpage.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const JoinMeeting(),
+      home: const LoginPage(),
     );
   }
 }
